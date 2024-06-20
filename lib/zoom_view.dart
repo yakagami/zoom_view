3e6c50dfd870649c154660fb594e6d9e1f1277ec
yakagami <92773903+yakagami@users.noreply.github.com>
2024-06-20 16:59:32 -0400

    add column with single expanded child to force layout to be better

--- lib/zoom_view.dart
+++ lib/zoom_view.dart
@@ -165,112 +165,118 @@ class _ZoomViewState extends State<ZoomView> {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-      double height = constraints.maxHeight;
-      double width = constraints.maxWidth;
-      return GestureDetector(
-        onScaleStart: (ScaleStartDetails details) {
-          if (details.pointerCount == 1) {
-            DragStartDetails dragDetails = DragStartDetails(
-                globalPosition: details.focalPoint,
-                kind: PointerDeviceKind.touch);
-            DragStartDetails hDragDetails = DragStartDetails(
-                globalPosition: details.focalPoint,
-                kind: PointerDeviceKind.touch);
-            handleDragStart(dragDetails);
-            hHandleDragStart(hDragDetails);
-          } else {
-            distanceFromOffset = details.localFocalPoint.dy;
-            horizontalDistanceFromOffset = details.localFocalPoint.dx;
-            focalPointDistanceFromBottomFactor =
-                (height - distanceFromOffset) / distanceFromOffset;
-            horizontalFocalPointDistanceFromBottomFactor =
-                (width - horizontalDistanceFromOffset) /
-                    horizontalDistanceFromOffset;
-          }
-        },
-        onScaleUpdate: (ScaleUpdateDetails details) {
-          if (details.pointerCount > 1) {
-            double oldHeight = height * scale;
-            double oldWidth = width * scale;
-            if (lastScale / details.scale <= 1.0) {
-              //print(scale);
-              setState(() {
-                scale = lastScale / details.scale;
-              });
-            }
-            //vertical offset
-            final double newHeight = height * scale;
-            controller.jumpTo(controller.offset +
-                (oldHeight - newHeight) /
-                    (1 + focalPointDistanceFromBottomFactor));
-
-            //horizontal offset
-            final double newWidth = width * scale;
-            horizontalController.jumpTo(horizontalController.offset +
-                (oldWidth - newWidth) /
-                    (1 + horizontalFocalPointDistanceFromBottomFactor));
-          } else {
-            final Duration currentTime =
-                Duration(milliseconds: DateTime.now().millisecondsSinceEpoch);
-            final double correctedDelta = details.focalPointDelta.dy * scale;
-            final Offset correctedOffset = details.focalPoint * scale;
-            tracker.addPosition(currentTime, correctedOffset);
-            final DragUpdateDetails verticalDetails = DragUpdateDetails(
-                globalPosition: correctedOffset,
-                sourceTimeStamp: currentTime,
-                primaryDelta: correctedDelta,
-                delta: Offset(0.0, correctedDelta));
-            final double horizontalCorrectedDelta =
-                details.focalPointDelta.dx * scale;
-            final DragUpdateDetails horizontalDetails = DragUpdateDetails(
-                globalPosition: correctedOffset,
-                sourceTimeStamp: currentTime,
-                primaryDelta: horizontalCorrectedDelta,
-                delta: Offset(horizontalCorrectedDelta, 0.0));
-            handleDragUpdate(verticalDetails);
-            hHandleDragUpdate(horizontalDetails);
-          }
-        },
-        onScaleEnd: (ScaleEndDetails details) {
-          lastScale = scale;
-          Offset velocity = tracker.getVelocity().pixelsPerSecond;
-          DragEndDetails endDetails = DragEndDetails(
-            velocity: Velocity(
-              pixelsPerSecond: Offset(0.0, velocity.dy),
-            ),
-            primaryVelocity: velocity.dy,
-          );
-          DragEndDetails hEndDetails = DragEndDetails(
-            velocity: Velocity(
-              pixelsPerSecond: Offset(velocity.dx, 0.0),
-            ),
-            primaryVelocity: velocity.dx,
-          );
-          handleDragEnd(endDetails);
-          hHandleDragEnd(hEndDetails);
-        },
-        child: FittedBox(
-          fit: BoxFit.fill,
-          child: ScrollConfiguration(
-            behavior: const ScrollBehavior().copyWith(
-                overscroll: false, dragDevices: <PointerDeviceKind>{}),
-            child: SizedBox(
-              height: height * scale,
-              width: width * scale,
-              child: ListView(
-                physics: const ClampingScrollPhysics(),
-                controller: horizontalController,
-                scrollDirection: Axis.horizontal,
-                children: [
-                  SizedBox(width: width, child: widget.child),
-                ],
+    return Column(
+      children: [
+        Expanded(
+          child: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+            double height = constraints.maxHeight;
+            double width = constraints.maxWidth;
+            return GestureDetector(
+              onScaleStart: (ScaleStartDetails details) {
+                if (details.pointerCount == 1) {
+                  DragStartDetails dragDetails = DragStartDetails(
+                      globalPosition: details.focalPoint,
+                      kind: PointerDeviceKind.touch);
+                  DragStartDetails hDragDetails = DragStartDetails(
+                      globalPosition: details.focalPoint,
+                      kind: PointerDeviceKind.touch);
+                  handleDragStart(dragDetails);
+                  hHandleDragStart(hDragDetails);
+                } else {
+                  distanceFromOffset = details.localFocalPoint.dy;
+                  horizontalDistanceFromOffset = details.localFocalPoint.dx;
+                  focalPointDistanceFromBottomFactor =
+                      (height - distanceFromOffset) / distanceFromOffset;
+                  horizontalFocalPointDistanceFromBottomFactor =
+                      (width - horizontalDistanceFromOffset) /
+                          horizontalDistanceFromOffset;
+                }
+              },
+              onScaleUpdate: (ScaleUpdateDetails details) {
+                if (details.pointerCount > 1) {
+                  double oldHeight = height * scale;
+                  double oldWidth = width * scale;
+                  if (lastScale / details.scale <= 1.0) {
+                    //print(scale);
+                    setState(() {
+                      scale = lastScale / details.scale;
+                    });
+                  }
+                  //vertical offset
+                  final double newHeight = height * scale;
+                  controller.jumpTo(controller.offset +
+                      (oldHeight - newHeight) /
+                          (1 + focalPointDistanceFromBottomFactor));
+          
+                  //horizontal offset
+                  final double newWidth = width * scale;
+                  horizontalController.jumpTo(horizontalController.offset +
+                      (oldWidth - newWidth) /
+                          (1 + horizontalFocalPointDistanceFromBottomFactor));
+                } else {
+                  final Duration currentTime =
+                      Duration(milliseconds: DateTime.now().millisecondsSinceEpoch);
+                  final double correctedDelta = details.focalPointDelta.dy * scale;
+                  final Offset correctedOffset = details.focalPoint * scale;
+                  tracker.addPosition(currentTime, correctedOffset);
+                  final DragUpdateDetails verticalDetails = DragUpdateDetails(
+                      globalPosition: correctedOffset,
+                      sourceTimeStamp: currentTime,
+                      primaryDelta: correctedDelta,
+                      delta: Offset(0.0, correctedDelta));
+                  final double horizontalCorrectedDelta =
+                      details.focalPointDelta.dx * scale;
+                  final DragUpdateDetails horizontalDetails = DragUpdateDetails(
+                      globalPosition: correctedOffset,
+                      sourceTimeStamp: currentTime,
+                      primaryDelta: horizontalCorrectedDelta,
+                      delta: Offset(horizontalCorrectedDelta, 0.0));
+                  handleDragUpdate(verticalDetails);
+                  hHandleDragUpdate(horizontalDetails);
+                }
+              },
+              onScaleEnd: (ScaleEndDetails details) {
+                lastScale = scale;
+                Offset velocity = tracker.getVelocity().pixelsPerSecond;
+                DragEndDetails endDetails = DragEndDetails(
+                  velocity: Velocity(
+                    pixelsPerSecond: Offset(0.0, velocity.dy),
+                  ),
+                  primaryVelocity: velocity.dy,
+                );
+                DragEndDetails hEndDetails = DragEndDetails(
+                  velocity: Velocity(
+                    pixelsPerSecond: Offset(velocity.dx, 0.0),
+                  ),
+                  primaryVelocity: velocity.dx,
+                );
+                handleDragEnd(endDetails);
+                hHandleDragEnd(hEndDetails);
+              },
+              child: FittedBox(
+                fit: BoxFit.fill,
+                child: ScrollConfiguration(
+                  behavior: const ScrollBehavior().copyWith(
+                      overscroll: false, dragDevices: <PointerDeviceKind>{}),
+                  child: SizedBox(
+                    height: height * scale,
+                    width: width * scale,
+                    child: ListView(
+                      physics: const ClampingScrollPhysics(),
+                      controller: horizontalController,
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        SizedBox(width: width, child: widget.child),
+                      ],
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ),
+            );
+          }),
         ),
-      );
-    });
+      ],
+    );
   }
 }
